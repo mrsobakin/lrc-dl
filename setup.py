c60ace5a6efea43c16123f04669a4c9b81fd124b
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2024-11-11 05:26:46 +0300

    Bump version to `0.1.0`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lrc_dl',
-    version='0.0.3',
+    version='0.1.0',
     description='An ultimate cli tool for downloading song lyrics, inspired by other awesome *-dl programs.',
     packages=[
         "lrc_dl",
